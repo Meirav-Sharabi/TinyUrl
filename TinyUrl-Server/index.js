0363a1c2aa9fd13d6673049d2c3f1339dc8049b4
Meirav-Sharabi <ms4190787@gmail.com>
2024-06-06 00:28:27 +0300

    first commit

---
+++ TinyUrl-Server/index.js
@@ -0,0 +1,22 @@
+import express from 'express'  
+import cors from "cors"
+import bodyParser from "body-parser";
+
+import LinkRouter from './routes/LinkRouter.js';
+import UserRouter from './routes/UserRouter.js';
+import connectDB from './database.js';
+
+connectDB();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/user",UserRouter);
+app.use("/link",LinkRouter);
+
+const port = 3000
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
